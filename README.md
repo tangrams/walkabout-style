3dd1aa767c6cfb86264ddcbe822015249250a8ca
geraldine <geraldine@mapzen.com>
2016-04-15 15:11:18 -0700

    init commit

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
-# outdoor-style
+# terrain-demos
 
-<img src="http://www.jeanstalk.com.tw/wp-content/uploads/2013/06/JTweb-D081108-01.jpg" width="256px">
+Ways to manipulate heightmap data in [Tangram](http://github.com/tangrams/tangram)
+
+## Style gallery
+
+- elevation tiles: http://tangrams.github.io/terrain-demos/?url=styles/elevation-tiles.yaml
+- basic green: http://tangrams.github.io/terrain-demos/?url=styles/green.yaml
+- animated contours: http://tangrams.github.io/terrain-demos/?url=styles/contours.yaml
+- grayscale hypsometric: http://tangrams.github.io/terrain-demos/?url=styles/grayscale.yaml
+- classic hypsometric: http://tangrams.github.io/terrain-demos/?url=styles/hypsometric.yaml
+- slopemap: http://tangrams.github.io/terrain-demos/?url=styles/slope.yaml
+- heightmap: http://tangrams.github.io/terrain-demos/?url=styles/heightmap.yaml
+- manual normal derivation: http://tangrams.github.io/terrain-demos/?url=styles/normals-manual.yaml
+- normal tiles: http://tangrams.github.io/terrain-demos/?url=styles/normals-tiles.yaml
+- single light: http://tangrams.github.io/terrain-demos/?url=styles/single-light.yaml
+- two lights: http://tangrams.github.io/terrain-demos/?url=styles/two-lights.yaml
+- three lights: http://tangrams.github.io/terrain-demos/?url=styles/three-lights.yaml
+- environment map: http://tangrams.github.io/terrain-demos/?url=styles/environment-map1.yaml
+- metal spheremap: http://tangrams.github.io/terrain-demos/?url=styles/metal.yaml
+- sunrise spheremap: http://tangrams.github.io/terrain-demos/?url=styles/sunrise.yaml
+- sunset spheremap: http://tangrams.github.io/terrain-demos/?url=styles/sunset.yaml
+- swiss style: http://tangrams.github.io/terrain-demos/?url=styles/imhof.yaml
+
+### To run locally:
+
+Start a web server in the repo's directory:
+
+    python -m SimpleHTTPServer 8000
+    
+If that doesn't work, try:
+
+    python -m http.server 8000
+    
+Then navigate to: [http://localhost:8000](http://localhost:8000)
---
+++ main.js
@@ -0,0 +1,84 @@
+/*jslint browser: true*/
+/*global Tangram, gui */
+
+function parseQuery (qstr) {
+    var query = {};
+    var a = qstr.split('&');
+    for (var i in a) {
+        var b = a[i].split('=');
+        query[decodeURIComponent(b[0])] = decodeURIComponent(b[1]);
+    }
+    return query;
+}
+
+map = (function () {
+    'use strict';
+
+    var map_start_location = [37.8090, -122.2220, 12]; // Oakland
+
+    /*** URL parsing ***/
+
+    // leaflet-style URL hash pattern:
+    // #[zoom],[lat],[lng]
+    var url_hash = window.location.hash.slice(1, window.location.hash.length).split('/');
+
+    if (url_hash.length == 3) {
+        map_start_location = [url_hash[1],url_hash[2], url_hash[0]];
+        // convert from strings
+        map_start_location = map_start_location.map(Number);
+    }
+
+    // determine the scene url and content to load during start-up
+    var scene_url = 'styles/sketch7-no-grid.yaml';
+
+    // If there is a query, use it as the scene_url
+    var query = parseQuery(window.location.search.slice(1));
+    if (query.url) {
+        scene_url = query.url;
+    }
+    /*** Map ***/
+
+    var southWest = L.latLng(31.95, -126.23),
+        northEast = L.latLng(44.09, -112.76),
+        bounds = L.latLngBounds(southWest, northEast);
+
+    var map = L.map('map',
+        {"keyboardZoomOffset" : .05, maxZoom: 20 }
+    );
+
+    var layer = Tangram.leafletLayer({
+        scene: scene_url,
+        attribution: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>'
+    });
+
+    if (query.quiet) {
+        layer.options.attribution = "";
+        map.attributionControl.setPrefix('');
+        window.addEventListener("load", function() {
+            var div = document.getElementById("mz-bug");
+            if (div != null) {div.style.display = "none";}
+            div = document.getElementById("mz-citysearch");
+            if (div != null) {div.style.display = "none";}
+            div = document.getElementById("mz-geolocator");
+            if (div != null) {div.style.display = "none";}
+        });
+    }
+
+    if (query.noscroll) {
+        map.scrollWheelZoom.disable();
+    }
+
+    window.layer = layer;
+    var scene = layer.scene;
+    window.scene = scene;
+
+    // setView expects format ([lat, long], zoom)
+    map.setView(map_start_location.slice(0, 3), map_start_location[2]);
+
+    var hash = new L.Hash(map);
+
+    layer.addTo(map);
+
+    return map;
+
+}());
